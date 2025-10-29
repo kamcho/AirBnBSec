df6dac85fd0ab2ad364a1b4adf935263912f7cac
kamcho <njokevin9@gmail.com>
2025-10-29 12:39:20 +0300

    Remove tracked migrations and pycache folders

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
