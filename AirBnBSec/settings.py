ef91813810171dc44ea01c8f79b84c5abc2b9e48
kamcho <njokevin9@gmail.com>
2025-10-29 10:31:32 +0300

    cors

--- AirBnBSec/settings.py
+++ AirBnBSec/settings.py
@@ -168,12 +168,29 @@ AUTH_USER_MODEL = 'users.MyUser'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Database configuration
+if os.getenv('ENVIRONMENT') == 'PRODUCTION':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_NAME', 'MyBnBSecurity$default'),
+            'USER': os.getenv('DB_USER', 'MyBnBSecurity'),
+            'PASSWORD': os.getenv('DB_PASSWORD', ''),  # Will be set in production
+            'HOST': os.getenv('DB_HOST', 'MyBnBSecurity.mysql.pythonanywhere-services.com'),
+            'PORT': os.getenv('DB_PORT', '3306'),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4',
+            },
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
